6dac5a5ce0d8f289ab490b80f69a393fa27774bf
mccairelaw <mccairelaw@gmail.com>
2021-05-04 10:06:25 -0700

    doxy comments

--- Processor.h
+++ Processor.h
@@ -10,7 +10,18 @@
 
 class Processor{
 public:
+    /**
+     * This is an abstract method for processing 8-bit audio buffers 
+     * @param buffer an unsigned char* audio buffer
+     * @param bufferSize the size of the audio buffer
+     */
     virtual void processBuffer(unsigned char* buffer, int bufferSize) = 0;
+    
+    /**
+     * This is an abstract method for processing 16-bit audio buffers 
+     * @param buffer a short* audio buffer
+     * @param bufferSize the size of the audio buffer
+     */
     virtual void processShortBuffer(short* shortBuffer, int bufferSize) = 0;
 };
 
